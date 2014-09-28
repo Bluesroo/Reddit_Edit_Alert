ed4e34d30cc86bb79e356dcaa481d19395b19a7d
bluesroo <jpariseau101@gmail.com>
2014-09-27 20:41:32 -0700

    Changed tabs to spaces.

--- editedAlert.py
+++ editedAlert.py
@@ -5,45 +5,48 @@ username = "blah"
 password = "blah"
 body = "This is just an alert that a comment that you previously responded to has been edited.\n\n^(This [bot](https://github.com/Bluesroo/Reddit_Edit_Alert) was made by /u/Bluesroo. Message him with any problems you have.)"
 
+
 def get_parent(r, child):
-	url = child.link_url
-	parent_id = child.parent_id.partition('_')
-	submission = r.get_submission(url + parent_id[2])
-	parent = submission.comments[0]
-	return parent
+    url = child.link_url
+    parent_id = child.parent_id.partition('_')
+    submission = r.get_submission(url + parent_id[2])
+    parent = submission.comments[0]
+    return parent
+
 
 def alert_children(parent):
-	children = parent.replies
-	for child in children:
-		if child.author != 'user_battle_bot':
-			if child.author != parent.author:
-				child.reply(body)
-	return
+    children = parent.replies
+    for child in children:
+        if child.author != 'user_battle_bot':
+            if child.author != parent.author:
+                child.reply(body)
+    return
+
 
 def main():
-	r = praw.Reddit("Edit alert bot by /u/Bluesroo")
-	print("Logging in...")
-	r.login(username, password)
+    r = praw.Reddit("Edit alert bot by /u/Bluesroo")
+    print("Logging in...")
+    r.login(username, password)
 
-	print("Initializing the lists...")
-	checked_list = []
-	edited_list = []
+    print("Initializing the lists...")
+    checked_list = []
+    edited_list = []
 
-	while True:
-		print("Getting fresh comments...")
-		comments = r.get_comments('all')
-		for comment in comments:
-			print(comment.author)
-			if comment.id not in checked_list:
-				parent_comment = get_parent(r, comment)
-				if parent_comment.name not in edited_list:
-					if parent_comment.edited:
-						print("Alerting children...")
-						alert_children(parent_comment)
-					edited_list.append(parent_comment.id)
-			checked_list.append(comment.id)
-		print("Sleeping...")
-		time.sleep(30)
+    while True:
+        print("Getting fresh comments...")
+        comments = r.get_comments('all')
+        for comment in comments:
+            print(comment.author)
+            if comment.id not in checked_list:
+                parent_comment = get_parent(r, comment)
+                if parent_comment.name not in edited_list:
+                    if parent_comment.edited:
+                        print("Alerting children...")
+                        alert_children(parent_comment)
+                edited_list.append(parent_comment.id)
+            checked_list.append(comment.id)
+        print("Sleeping...")
+        time.sleep(30)
 
 if __name__ == "__main__":
-	main()
+    main()
